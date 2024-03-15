5925461c6eb2846593a000b35492ce51e70feb8a
gaganparmar110 <gaganparmar110@gmail.com>
2024-03-15 11:47:31 +0530

    Added enviroment files for staging.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 
 function App() {
+  const handleSubmit = () => {
+    fetch(
+      `${process.env.REACT_APP_DEV_API_URL}student/submission_dashboard/7`
+    ).then((res) => {
+      console.log("Res:", res);
+    });
+  };
   return (
     <div className="App">
-      <h1>First - CI/CD Build Success</h1>
+      <h1>Second - CI/CD Build Success</h1>
+      <button onClick={() => handleSubmit()}>Ok</button>
     </div>
   );
 }
